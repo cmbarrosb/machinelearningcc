97220e0c3d3f5b7a6d8fdd3de035ac944f756236
EC2 Default User <ec2-user@ip-10-0-1-117.ec2.internal>
2025-05-08 02:11:23 +0000

    updated instance files

--- install_deps.sh
+++ install_deps.sh
@@ -29,9 +29,19 @@ source venv/bin/activate
 
 # ---- Install packages ----
 pip install --upgrade pip
-pip install torch torchvision matplotlib pandas
+# Free pip cache to save space
+pip cache purge
 
-# ---- Freeze versions ----
+# Install CPU-only PyTorch and TorchVision
+pip install torch==1.13.1+cpu torchvision==0.14.1+cpu \
+  -f https://download.pytorch.org/whl/cpu/torch_stable.html
+pip install matplotlib pandas
+pip install "urllib3<2.0"
 pip freeze > requirements.txt
+# Clear pip cache again
+pip cache purge
+
+pip freeze > requirements.txt
+# ---- Freeze versions ----
 
 echo "===== Dependency install finished: $(date) ====="
